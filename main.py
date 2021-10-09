922478df7490f28596675a742c71184f4fda2e81
suslik <dunordavind@protonmail.com>
2021-10-09 14:23:50 +0200

    Implemented contrast summary endpoint

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # This is a sample Python script.
-import os
-import re
-from collections import OrderedDict
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import numpy as np
+import os
 import tiledb
 
 app = FastAPI()
-index = "studies"
 
 origins = ["*"]
 
@@ -23,7 +20,7 @@ app.add_middleware(
 database_uri = os.path.join("/home/suslik/Documents/programming/envision/backend/middle_layer/latest/database")
 
 
-def get_uri_for_study(study_id: str):
+def tdb_uri_for_study(study_id: str):
     """
 
     :param study_id:
@@ -33,7 +30,7 @@ def get_uri_for_study(study_id: str):
     return os.path.join(database_uri, study_id)
 
 
-def get_uri_for_contrasts(study_id: str):
+def tdb_uri_for_contrasts(study_id: str):
     """
 
 
@@ -41,18 +38,31 @@ def get_uri_for_contrasts(study_id: str):
     :return: Path to the contrast array for the specified study
     """
 
-    return os.path.join(database_uri, study_id, "contrasts")
+    return os.path.join(tdb_uri_for_study(study_id), "contrasts")
 
 
-async def get_contrasts_for_study(study_id):
-    uri = get_uri_for_contrasts(study_id)
+def tdb_uri_for_stats(study_id: str):
+    return os.path.join(tdb_uri_for_study(study_id), "stats")
+
+
+async def tdb_get_contrasts_for_study(study_id):
+    uri = tdb_uri_for_contrasts(study_id)
     with tiledb.open(uri, 'r') as A:
-        a = tiledb.QueryCondition(expression="logFC > 0")
-        schema = A.schema
+        # a = tiledb.QueryCondition(expression="logFC > 0")
         contrasts = A[:]
         contrasts = dict(zip(contrasts['contrasts'],
                              contrasts['formula']))
     return contrasts
+
+
+async def tdb_get_studies(to_bytes: bool = False):
+    res = []
+    tiledb.ls(database_uri, lambda obj_path, obj_type: res.append(obj_path))
+    res = [os.path.basename(os.path.normpath(x)) for x in res]
+    if to_bytes:
+        res = [bytes(str(x), 'utf-8') for x in res]
+
+    return res
 
 
 @app.get("/")
@@ -61,20 +71,22 @@ async def root():
 
 
 @app.get("/studies/")
-async def studies():
+async def api_get_studies():
     # TODO: implement checks for path existing
 
-    res = []
-    tiledb.ls(database_uri, lambda obj_path, obj_type: res.append(obj_path))
-    res = [os.path.basename(os.path.normpath(x)) for x in res]
+    res = await tdb_get_studies()
 
     return {"message": tiledb.object_type(database_uri), "res": res}
 
 
 @app.get("/studies/{study_id}/")
-async def study_by_id(study_id: str):
-    # TODO implement checks for study path existing
-    contrasts = await get_contrasts_for_study(study_id)
+async def api_get_study_by_id(study_id: str):
+    # check that study can be found
+    studies = await tdb_get_studies()
+    if study_id not in studies:
+        raise HTTPException(status_code=404,
+                            detail=f"Study {study_id} not available in the database")
+    contrasts = await tdb_get_contrasts_for_study(study_id)
     res = {"contrasts": contrasts}
 
     return {"message": "GET /studies/{study_id}/: the api is responding",
@@ -83,20 +95,40 @@ async def study_by_id(study_id: str):
 
 
 @app.get("/studies/{study_id}/contrasts")
-async def contrasts_for_study(study_id):
-    return {"message": "GET /studies/{study_id}/contrasts: the api is responding"}
+async def api_get_contrasts_for_study(study_id: str):
+    res = await api_get_study_by_id(study_id)
+    return res
 
 
-@app.get("/studies/{study_id}/contrasts/{contrast_name}/")
-async def contrast_summary(study_id, contrast_name):
-    return {"message": "GET /studies/{study_id}/contrasts/{contrast_name}/: the api is responding"}
+@app.get("/studies/{study_id}/contrasts/{contrast_id}/")
+async def api_get_contrast_summary(study_id: str, contrast_id: bytes):
 
+    # check that study and contrast can be found
+    studies = await tdb_get_studies()
+    if study_id not in studies:
+        raise HTTPException(status_code=404,
+                            detail=f"Study {study_id} not available in the database")
 
-@app.get("/studies/{study_id}/contrasts/{contrast_name}/geneCount/")
-async def n_genes_per_contrast(study_id):
-    return {"message": "GET /studies/{study_id}/contrasts/{contrast_name}/geneCount/: the api is responding"}
+    contrasts = dict(await tdb_get_contrasts_for_study(study_id))
+    if contrast_id not in contrasts:
+        raise HTTPException(status_code=404,
+                            detail=f"Contrast {contrast_id} not available for the study {study_id}")
 
+    # 2 - TODO Calculate total number of genes for the study
 
-@app.get("/studies/{study_id}/contrasts/pvalues/")
-async def study_by_id(study_id):
-    return {"message": "GET /studies/{study_id}/contrasts/pvalues/: the api is responding"}
+    uri = tdb_uri_for_stats(study_id)
+    with tiledb.open(uri, 'r') as A:
+        pvalues = A[:, contrast_id]['pvalue']
+        fdr = A[:, contrast_id]['fdr']
+
+    print(np.where(pvalues < 0.05))
+
+    res = {
+        "n_genes": len(pvalues),
+        "n_fdr_significant_genes": len(np.where(fdr < 0.05)[0]),
+        "n_p_significant_genes": len(np.where(pvalues < 0.05)[0])
+    }
+    # 3 - TODO Calculate total number of genes with p < 0.05 (might not necessary for this endpoint)
+
+    return {"message": "GET /studies/{study_id}/contrasts/{contrast_name}/: the api is responding",
+            "res": res}
